bb413ea9ca108f6fc4949f76def64928fac1f587
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-06-12 17:00:03 -0600

    Add experience section and add/remove functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Component } from 'react';
 import General from './components/General';
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       eduInstances: [{ key: 0 }],
+      expInstances: [{ key: 0 }],
     };
 
     this.addEdu = this.addEdu.bind(this);
     this.delEdu = this.delEdu.bind(this);
+    this.addExp = this.addExp.bind(this);
+    this.delExp = this.delExp.bind(this);
   }
 
   addEdu() {
@@ -30,8 +34,25 @@ class App extends Component {
     }));
   }
 
+  addExp() {
+    this.setState((prevState) => ({
+      expInstances: prevState.expInstances.concat({
+        key: prevState.expInstances.length,
+      }),
+    }));
+  }
+
+  delExp() {
+    this.setState((prevState) => ({
+      expInstances: prevState.expInstances.slice(
+        0,
+        prevState.expInstances.length - 1
+      ),
+    }));
+  }
+
   render() {
-    const { eduInstances } = this.state;
+    const { eduInstances, expInstances } = this.state;
     return (
       <div className="App">
         <General />
@@ -42,6 +63,16 @@ class App extends Component {
           Add section
         </button>
         <button type="button" onClick={this.delEdu}>
+          Remove
+        </button>
+
+        {expInstances.map((item) => (
+          <Experience key={item.key} />
+        ))}
+        <button type="button" onClick={this.addExp}>
+          Add section
+        </button>
+        <button type="button" onClick={this.delExp}>
           Remove
         </button>
       </div>
